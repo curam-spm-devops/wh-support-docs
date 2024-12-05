571d3cb8788f2bc91e1cdb710cac5c8318425c86
bridgetfPublic <162606734+bridgetfPublic@users.noreply.github.com>
2024-12-05 21:25:01 +0000

    Update index.js

--- src/gatsby-theme-carbon/components/Header/index.js
+++ src/gatsby-theme-carbon/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from 'gatsby-theme-carbon/src/components/Header';
+import Logo from "../../../images/curam-icon-512.png";
 
 const CustomHeader = (props) => (
   <Header {...props}>
