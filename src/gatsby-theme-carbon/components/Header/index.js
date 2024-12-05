d24a11e5de81a8539473eba5c21482ba10143015
bridgetfPublic <162606734+bridgetfPublic@users.noreply.github.com>
2024-12-05 21:53:34 +0000

    Update index.js

--- src/gatsby-theme-carbon/components/Header/index.js
+++ src/gatsby-theme-carbon/components/Header/index.js
@@ -4,6 +4,7 @@ import Logo from "../../../images/curam-icon-512.png";
 
 const CustomHeader = (props) => (
   <Header {...props}>
+   <img src={Logo} style={{ width: "24px", marginInlineEnd: "0.5rem" }} />
     Merative Support&nbsp;<span>Docs</span>&nbsp;(transitional)
   </Header>
 );
